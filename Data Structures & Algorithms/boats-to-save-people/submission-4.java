77d18e94a0eefe24244cbedd6153fcbb608ee213
RUHAN K. B. <ruhankb29@gmail.com>
2026-05-17 10:14:25 +0530

    Add: boats-to-save-people - submission-4

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0;
+        int j = people.length - 1;
+        int ans = 0;
+        while(i<=j)
+        {
+            int tot = people[i] + people[j];
+            if(tot <= limit) i++;
+            j--;
+            ans++;
+        }
+        return ans;
+    }
+}
