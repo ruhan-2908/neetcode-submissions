67c125dcc5317154ee66d5f73e83079c59edb2d2
RUHAN K. B. <ruhankb29@gmail.com>
2026-05-18 18:03:40 +0530

    Add: search-in-rotated-sorted-array-ii - submission-9

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-9.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int i  = 0;
+        int j = nums.length - 1;
+        while( i <= j )
+        {
+            int mid = i  + (j-i)/2;
+            if(nums[mid] == target) return true;
+            if( nums[i] < nums[mid])
+            {
+                if(nums[i] <= target && target < nums[mid])
+                {
+                    j = mid - 1;
+                }else
+                {
+                    i = mid + 1;
+                }
+            }else if ( nums[i] > nums[mid])
+            {   
+                if(nums[mid] < target && target <= nums[j])
+                {
+                    i = mid + 1;
+                }else
+                {
+                    j = mid - 1;
+                }
+            }else
+            {
+                i += 1;
+            }
+        }
+        return false;
+    }
+}
