bd0a552e2516f2f4a690dce6491c16d23eaf1c04
RUHAN K. B. <ruhankb29@gmail.com>
2026-05-20 15:47:00 +0530

    Update: design-twitter-feed - submission-5

--- Data Structures & Algorithms/design-twitter-feed/submission-5.java
+++ Data Structures & Algorithms/design-twitter-feed/submission-5.java
@@ -1,54 +1,57 @@
 class Twitter {
-    private int count;
-    private HashMap<Integer,List<int[]>> tweetMap;
-    private HashMap<Integer,Set<Integer>> followMap;
+    HashMap<Integer,HashSet<Integer>> followMap;
+    HashMap<Integer,List<int[]>> tweetMap;
+    int count;
     public Twitter() {
-        count = 0;
         tweetMap = new HashMap<>();
         followMap = new HashMap<>();
+        this.count = 0;
     }
     
     public void postTweet(int userId, int tweetId) {
-        tweetMap.computeIfAbsent(userId, k -> new ArrayList<>()).add(new int[]{count--,tweetId});
+        tweetMap.computeIfAbsent(userId,k-> new ArrayList<>()).add(new int[]{count--,tweetId});
     }
     
     public List<Integer> getNewsFeed(int userId) {
-        List<Integer> res = new ArrayList<>();
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
+        List<Integer> ans = new ArrayList<>();
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
 
-        followMap.computeIfAbsent(userId, k -> new HashSet<>()).add(userId);
-        for(int tempId : followMap.get(userId))
+        followMap.computeIfAbsent(userId,k-> new HashSet<>()).add(userId);
+
+        for(int following : followMap.get(userId))
         {
-            if(tweetMap.containsKey(tempId))
+            if(tweetMap.containsKey(following))
             {
-                List<int[]> tweets = tweetMap.get(tempId);
-                int index = tweets.size() - 1;
-                int[] tweet = tweets.get(index);
-                minHeap.offer(new int[]{tweet[0],tweet[1],tempId,index});
+                List<int[]> tweets = tweetMap.get(following);
+                int length = tweets.size() - 1;
+                int[] tweet = tweets.get(length);
+                int recency = tweet[0];
+                int tweetId = tweet[1];
+                minHeap.offer(new int[]{recency,tweetId,following,length});
             }
         }
 
-        while(!minHeap.isEmpty() && res.size() < 10)
+        while(!minHeap.isEmpty() && ans.size() < 10)
         {
-            int[] tweet = minHeap.poll();
-            res.add(tweet[1]);
-            int length = tweet[3];
+            int[] temp = minHeap.poll();
+            ans.add(temp[1]);
+            int length = temp[3];
             if(length > 0)
             {
-                int[] tempTweet = tweetMap.get(tweet[2]).get(length-1);
-                minHeap.offer(new int[]{tempTweet[0],tempTweet[1],tweet[2],length-1});
+                int[] tweet = tweetMap.get(temp[2]).get(length-1);
+                length--;
+                minHeap.offer(new int[]{tweet[0],tweet[1],temp[2],length});
             }
         }
-
-        return res;
+        return ans;
     }
     
     public void follow(int followerId, int followeeId) {
-        followMap.computeIfAbsent(followerId, k -> new HashSet<>()).add(followeeId);
+        followMap.computeIfAbsent(followerId,k-> new HashSet<>()).add(followeeId);
     }
     
     public void unfollow(int followerId, int followeeId) {
-        followMap.computeIfPresent(followerId, (k,v) -> {
+        followMap.computeIfPresent(followerId,(k,v)->{
             v.remove(followeeId);
             return v;
         });
