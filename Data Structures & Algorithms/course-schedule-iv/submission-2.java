8faf7b400e044cb0488e7fd1258864feb3b3504b
RUHAN K. B. <ruhankb29@gmail.com>
2026-06-02 20:59:34 +0530

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    private List<Integer>[] home;
+    private HashMap<Integer,Set<Integer>> preMap;
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        home = new ArrayList[numCourses];
+        preMap = new HashMap<>();
+        for(int i = 0 ; i < numCourses ; i++)
+        {
+            home[i] = new ArrayList<Integer>();
+        }
+        for(int[] pre : prerequisites)
+        {
+            home[pre[1]].add(pre[0]);
+        }
+        for(int i = 0 ; i < numCourses ; i++)
+        {
+            dfs(i);
+        }
+        List<Boolean> ans = new ArrayList<>();
+        for(int[] q : queries)
+        {
+            ans.add(preMap.get(q[1]).contains(q[0]));
+        }
+        return ans;
+    }
+    public Set<Integer> dfs(int i)
+    {
+        if(preMap.containsKey(i)) return preMap.get(i);
+        Set<Integer> temp = new HashSet<>();
+        for(int nei : home[i])
+        {
+            temp.addAll(dfs(nei));
+        }
+        temp.add(i);
+        preMap.put(i,temp);
+        return temp;
+    }
+}
