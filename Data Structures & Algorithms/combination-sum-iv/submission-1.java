1c1ae8f8dd66ebf2fbd0aff84f82d7ff5e0770e1
RUHAN K. B. <ruhankb29@gmail.com>
2026-06-16 21:02:28 +0530

    Add: combination-sum-iv - submission-1

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(target,1);
+        for(int total = target ; total >= 0 ; total--)
+        {
+            for(int num : nums)
+            {
+                if(total < num) break;
+                map.put(total - num , map.getOrDefault(total,0) + map.getOrDefault(total - num,0));
+            }
+        }
+        return map.getOrDefault(0,0);
+    }
+}
