c12c3f012b118e5c54f7589c3251c880deefc3b5
RUHAN K. B. <ruhankb29@gmail.com>
2026-06-23 20:37:09 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int num = 0;
+        for(int n : nums)
+        {
+            num = num ^ n;
+        }
+        return num;
+    }
+}
